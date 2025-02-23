4095aa04236fddda6665e4b0162ec51e71689c85
Josh Burgoyne <jburgoyne86@gmail.com>
2025-02-22 19:16:28 -0700

    Create userdata and userservice classes

--- server/src/main/java/server/handlers/RegisterHandler.java
+++ server/src/main/java/server/handlers/RegisterHandler.java
@@ -2,10 +2,15 @@ package server.handlers;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import com.google.gson.Gson;
+import model.UserData;
 
 public class RegisterHandler implements Route {
     @Override
     public Object handle(Request req, Response res) {
+        var serializer = new Gson();
+        var userData = new Gson().fromJson(req.body(), UserData.class);
+
         return "Hello there!";
     }
 }
