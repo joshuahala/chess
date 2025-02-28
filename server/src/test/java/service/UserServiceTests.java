007cf2e29f32f8d90f927981493261d42eeaad4c
Josh Burgoyne <jburgoyne86@gmail.com>
2025-02-27 20:16:18 -0700

    Write tests for registering and deleting users

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -10,13 +10,31 @@ public class UserServiceTests {
     @DisplayName("Register User")
     public void registerNewUser() throws DataAccessException {
         UserService service = new UserService();
-        String json = "{\"username\":\"Jeremy\",\"password\":\"secret\",\"email\":\"jeremy@gmail.com\"}";
-        var userData = new Gson().fromJson(json, UserData.class);
+        String jsonInput = "{\"username\":\"Jeremy\",\"password\":\"secret\",\"email\":\"jeremy@gmail.com\"}";
+        String expectedName = "Jeremy";
+
+        var userData = new Gson().fromJson(jsonInput, UserData.class);
         RegisterResult registerResult = service.register(userData);
+        var resultName = registerResult.username();
+        var resultToken = registerResult.authToken();
 
-        var result = new Gson().toJson(registerResult);
-
-        Assertions.assertEquals(json, result,
+        Assertions.assertEquals(expectedName, resultName,
                 "These are not the same, bro");
+        Assertions.assertInstanceOf(String.class,resultToken);
+    }
+    @Test
+    @DisplayName("Get User Info")
+    public void getUserInfo() throws DataAccessException {
+        UserService service = new UserService();
+        String jsonInput = "{\"username\":\"Jeremy\",\"password\":\"secret\",\"email\":\"jeremy@gmail.com\"}";
+
+        var userData = new Gson().fromJson(jsonInput, UserData.class);
+        UserData expectedUser = new Gson().fromJson(jsonInput, UserData.class);
+        RegisterResult registerResult = service.register(userData);
+        UserData retrievedUser = service.userDAO.getUser("Jeremy");
+
+        Assertions.assertEquals(expectedUser, retrievedUser,
+                "This is not the user you are looking for");
+
     }
 }
