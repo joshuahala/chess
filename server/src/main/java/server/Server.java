1b3fcca4996729f40aa9d2df3f728c0d57f33d2b
Josh Burgoyne <jburgoyne86@gmail.com>
2025-02-27 23:10:27 -0700

    Implement server error handling

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,7 +1,10 @@
 package server;
 
+import com.google.gson.Gson;
 import spark.*;
 import server.handlers.*;
+
+import java.util.Map;
 
 public class Server {
 
@@ -11,8 +14,10 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.exception(Exception.class, this::errorHandler);
         Spark.post("/user", new RegisterHandler());
         Spark.delete("/db", new ClearHandler());
+        Spark.post("/session", new LoginHandler());
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
@@ -24,5 +29,13 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    public Object errorHandler(Exception e, Request req, Response res) {
+        var body = new Gson().toJson(Map.of("message", String.format("Error: %s", e.getMessage()), "success", false));
+        res.type("application/json");
+        res.status(500);
+        res.body(body);
+        return body;
     }
 }
