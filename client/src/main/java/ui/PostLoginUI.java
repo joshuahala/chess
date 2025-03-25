19ff5667f182eb11d998890347dc972b653f4ce4
Josh Burgoyne <jburgoyne86@gmail.com>
2025-03-25 00:45:32 -0600

    implement postlogin help command.

--- client/src/main/java/ui/PostLoginUI.java
+++ client/src/main/java/ui/PostLoginUI.java
@@ -15,6 +15,9 @@ public class PostLoginUI implements ClientUI{
     ServerFacade server = new ServerFacade(8080);
     public ClientResult eval(String arg) throws ResponseException {
         switch (arg) {
+            case "help" -> {
+                return help();
+            }
             case "logout" -> {
                 return logout();
             }
@@ -33,6 +36,21 @@ public class PostLoginUI implements ClientUI{
         } catch (Exception error) {
             return new ClientResult(ClientType.POSTLOGIN, "", "There was an error");
         }
+    }
+    private ClientResult help() {
+        try {
+            var text ="" + EscapeSequences.SET_TEXT_COLOR_BLUE + "create <NAME> - a game%n" +
+                    "list - games%n" +
+                    "join <ID> [WHITE|BLACK] - a game%n" +
+                    "observe <ID> - a game%n" +
+                    "logout - when you are done%n" +
+                    "quit - playing chess%n" +
+                    "help - with possible commands%n";
+            return new ClientResult(ClientType.POSTLOGIN, "", text);
+        } catch (Exception error) {
+            return new ClientResult(ClientType.POSTLOGIN, "", "" + error);
+        }
+
     }
     private ClientResult defaultResponse() {
         return new ClientResult(ClientType.POSTLOGIN,"", "type something real punk");
